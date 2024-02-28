457667a09896192569d67b1fe2f0868853592ea6
ChristianPartida <christian.partida175627@potros.itson.edu.mx>
2024-02-27 20:17:06 -0700

    Practica completa dia 27 martes

---
+++ app/src/main/java/mx/edu/potros/practica6/MainActivity.kt
@@ -0,0 +1,18 @@
+package mx.edu.potros.practica6
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button :Button =findViewById(R.id.buttonP)
+        button.setOnClickListener(){
+            var intent: Intent=Intent(this,Catalogo::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/mx/edu/potros/practica6/detalle_pelicula.kt
@@ -0,0 +1,27 @@
+package mx.edu.potros.practica6
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class detalle_pelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pelicula)
+
+        val iv_pelicula_imagen: ImageView = findViewById(R.id.iv_pelicula_imagen)
+        val NombreSeriePelicula: TextView = findViewById(R.id.NombreSeriePelicula)
+        val tvserie_pelicula_Desc: TextView = findViewById(R.id.tvserie_pelicula_Desc)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            iv_pelicula_imagen.setImageResource(bundle.getInt("imagen"))
+            NombreSeriePelicula.text = bundle.getString("Titulo")
+            tvserie_pelicula_Desc.text = bundle.getString("Sinopsis")
+        }
+
+
+    }
+}
